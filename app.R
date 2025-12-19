6ab5ee89d2ec344bc1066bf0f54b77afa6d5a02c
Gwen Junod <gwen.junod@gmail.com>
2025-12-19 07:22:27 +0100

    Initial commit

--- app.R
+++ app.R
@@ -273,7 +273,7 @@ ui <- fluidPage(
           ),
           br(),
           h4("Copyrights and Reproducibility"),
-          p("CC info"),
+          p(HTML('This work is licensed under <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY-NC-SA 4.0</a><img src="https://mirrors.creativecommons.org/presskit/icons/cc.svg" alt="" style="max-width: 1em;max-height:1em;margin-left: .2em;"><img src="https://mirrors.creativecommons.org/presskit/icons/by.svg" alt="" style="max-width: 1em;max-height:1em;margin-left: .2em;"><img src="https://mirrors.creativecommons.org/presskit/icons/nc.svg" alt="" style="max-width: 1em;max-height:1em;margin-left: .2em;"><img src="https://mirrors.creativecommons.org/presskit/icons/sa.svg" alt="" style="max-width: 1em;max-height:1em;margin-left: .2em;">')),
           p("Main packages versions:"),
           tags$ul(
             tags$li("mlt: 1.7-1"),
